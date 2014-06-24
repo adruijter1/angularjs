9994ef4a4e5b854cecab9f5fdebb2a1997fd2aa5
Arjan de Ruijter <rra@mboutrecht.nl>
2014-06-24 09:01:33 +0200

    Tutorial 2.3

--- app.js
+++ app.js
@@ -18,6 +18,26 @@
 		persoonBestaatNiet: false,
 		saldoBankrekening: 78,
 		geboortedatum: "1968-08-30",
+		review: [
+			{
+				stars: 5,
+				body: "Dit is een aardig persoon",
+				author: "wdevries@hotmail.com",
+				newsletter: "checked"
+			},
+			{
+				stars: 1,
+				body: "Nederland gaat winnen vanavond",
+				author: "wvanbemmelen@gmail.com",
+				newsletter: ""
+			},
+			{
+				stars: 3,
+				body: "Chili gaat winnen vanavond",
+				author: "buenosdias@live.com",
+				newsletter: ""
+			}		
+		]
 	},
 	{
 		voornaam: "Louis",
@@ -28,7 +48,8 @@
 		klikOpMij: true,
 		persoonBestaatNiet: false,
 		saldoBankrekening: 123.45,
-		geboortedatum: "2005-08-03"
+		geboortedatum: "2005-08-03",
+		
 	},
 	{
 		voornaam: "Robin",
