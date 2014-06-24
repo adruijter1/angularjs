5ebecb05d6e554fea31ca0e78bf9b1ff2a05bb75
Arjan de Ruijter <rra@mboutrecht.nl>
2014-06-24 11:36:44 +0200

    Werkent submit formulier

--- app.js
+++ app.js
@@ -7,6 +7,15 @@
 		this.gegevens = naw;	
 	});
 	
+	app.controller("ReviewController", function(){
+		this.review = {};
+		
+		this.addReview = function(product){
+			product.review.push(this.review);
+			this.review = {};
+		};
+	});
+	
 	// Let op javascript is hoofdlettergevoelig
 	var naw = [{
 		voornaam: "Arjan",
@@ -14,10 +23,14 @@
 		achternaam: "Ruijter",
 		geboortejaar: 1990,
 		woonplaats: "Castricum",
-		klikOpMij: true,
+		klikOpMij: false,
 		persoonBestaatNiet: false,
 		saldoBankrekening: 78,
 		geboortedatum: "1968-08-30",
+		images: {
+				full: "images/Desert.jpg",
+				thumb: "images/thumbnail/tn_Desert.jpg"
+		},
 		review: [
 			{
 				stars: 5,
@@ -49,7 +62,10 @@
 		persoonBestaatNiet: false,
 		saldoBankrekening: 123.45,
 		geboortedatum: "2005-08-03",
-		
+		images: {
+				full: "images/Komodo_dragon_stalking_deer.png",
+				thumb: "images/thumbnail/tn_Komodo_dragon_stalking_deer.png"
+		}		
 	},
 	{
 		voornaam: "Robin",
@@ -60,7 +76,11 @@
 		klikOpMij: true,
 		persoonBestaatNiet: false,
 		saldoBankrekening: 12000000.15,
-		geboortedatum: "1982-12-21"
+		geboortedatum: "1982-12-21",
+		images: {
+				full: "images/Im-watching-you-meme.gif",
+				thumb: "images/thumbnail/tn_Im-watching-you-meme.gif"
+		}		
 	},
 	{
 		voornaam: "Arjan",
@@ -71,6 +91,10 @@
 		klikOpMij: true,
 		persoonBestaatNiet: false,
 		saldoBankrekening: 23000000.01,
-		geboortedatum: "1984-01-23"
+		geboortedatum: "1984-01-23",
+		images: {
+				full: "images/Kill Bill Vol 2 (2004) 3.jpg",
+				thumb: "images/thumbnail/tn_Kill Bill Vol 2 (2004) 3.jpg"
+		}
 	}];
 })();
